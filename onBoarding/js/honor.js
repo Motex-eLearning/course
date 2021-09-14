3d48476b7ba1443f359212993b74613d5164ab9b
Motex-eLearning <84168292+Motex-eLearning@users.noreply.github.com>
2021-09-14 09:23:15 +0800

    create timeLine

---
+++ onBoarding/js/honor.js
@@ -0,0 +1,80 @@
+import data from "./data.js"
+let filterData = data.filter(item => item.tag === "認證與獲獎");
+console.log(data);
+const nav = document.querySelector('.timeline-nav');
+const caption = document.querySelector('.timeline-slider');
+const btns = document.querySelector('.btn-group');
+
+
+function render(arr) {
+    let paginationStr = "";
+    let slideStr = "";
+    arr.forEach(item => {
+        paginationStr += `<div class="timeline-nav__item">${item.year}</div>`;
+        slideStr += `<div class="timeline-slide" style="background-image: url(https://unsplash.it/1920/600?image=11" data-year="${item.year}">      <span class="timeline-year">${item.year}</span>
+    <div class="timeline-slide__content">
+      <h4 class="timeline-title">${item.event}</h4>
+      <p class="timeline-text">${item.text}</p>
+    </div>
+  </div>`
+    });
+    nav.innerHTML = paginationStr;
+    caption.innerHTML = slideStr;
+}
+
+
+function slick() {
+    $('.timeline-nav').slick({
+        slidesToShow: 12,
+        slidesToScroll: 1,
+        asNavFor: '.timeline-slider',
+        centerMode: false,
+        focusOnSelect: true,
+        mobileFirst: true,
+        arrows: false,
+        infinite: false,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 8,
+                }
+            },
+            {
+                breakpoint: 0,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
+                }
+            }
+        ]
+    });
+    $('.timeline-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.timeline-nav',
+        centerMode: true,
+        cssEase: 'ease',
+        edgeFriction: 0.5,
+        mobileFirst: true,
+        speed: 500,
+        responsive: [{
+                breakpoint: 0,
+                settings: {
+                    centerMode: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: true
+                }
+            }
+        ]
+    });
+}
+
+$(function () {
+    render(filterData);
+    slick();
+});
